de4d4f27df312a9c16b7c72ab0773e83ed8fc267
virajkakade19 <virajkakade19@gmail.com>
2024-04-05 00:39:47 +0530

    yuhuuu

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { loginUser, signUpUser, logout, sendOTP } = require('../controller/user.js');
 const { generateToken } = require('../services/authentication.js');
+const User = require('../models/User.js');
 
 
 router.post('/login', loginUser);
@@ -9,13 +10,17 @@ router.post('/login', loginUser);
 router.post('/signup', signUpUser, sendOTP);
 
 router.post("/verify/otp", async(req,res)=>{
-    if(req.cookie.otp !== req.body.otp.json()) {
-        res.json({"ok":false});
-    };
-    await req.newUser.save();
-    const token = generateToken(req.newUser);
+    if(req.cookies.otp !== req.body.otp) {
+        return res.json({"ok":false});
+    }
+    console.log("otp here");
+    console.log(req.cookies.userData);
+    const data = req.cookies.userData;
+    const user = new User(data);
+    await user.save();
+    const token = generateToken(data);
     res.cookie('authToken', token, { httpOnly: true });
-    res.json({"ok" : true});
+    return res.clearCookie('otp').clearCookie('userData').json({"ok" : true});
 })
 router.get('/logout', logout);
 
